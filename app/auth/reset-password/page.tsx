a837f3a06b89b40ff12f48d7737805a4c16190ae
Chromeo <kurtcuff@gmail.com>
2025-11-10 17:08:21 -0800

    fix: make reset password page public
    - Remove ProtectedRoute wrapper from reset-password page
    - Users must access this page via magic link with valid token
    - Fixes 401 error preventing password reset

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,17 +1,15 @@
 /**
  * Reset Password Page
  * Allows users to set a new password after clicking email link
+ * Note: This is a public page - users access it via magic link from email
  */
 
 import { ResetPasswordForm } from '@/components/auth/ResetPasswordForm'
-import { PublicRoute } from '@/lib/components/ProtectedRoute'
 
 export default function ResetPasswordPage() {
   return (
-    <PublicRoute>
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 px-4 sm:px-6 lg:px-8">
-        <ResetPasswordForm />
-      </div>
-    </PublicRoute>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 px-4 sm:px-6 lg:px-8">
+      <ResetPasswordForm />
+    </div>
   )
 }
